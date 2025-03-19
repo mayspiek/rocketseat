adf35516b706f729cd7cfc6e5f7d6dd81ff7f18d
Mayara <maya.spieker@gmail.com>
2025-03-19 13:19:14 -0300

    feat: reset form

--- ignite/02-ignite-timer/src/pages/Home/index.tsx
+++ ignite/02-ignite-timer/src/pages/Home/index.tsx
@@ -14,7 +14,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -25,6 +25,8 @@ export function Home() {
 
   function handleCreateNewCycle(data : NewCycleFormData) {
     console.log(data)
+    // o reset coloca os valores que tao no defaultvalues
+    reset()
   }
 
   const task = watch("task");
